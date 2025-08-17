ec5f2d5a7093400d68a2ef4471f9253cac50ef5c
Laya Pandey <layapandey7171@gmail.com>
2025-08-17 22:30:26 +0545

    chatbot completed

--- backend/llm.py
+++ backend/llm.py
@@ -1,40 +1,27 @@
-from langchain.prompts import PromptTemplate
-from langchain_ollama import OllamaLLM
-from langgraph.graph import StateGraph , START, END
-from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage
+from langchain_ollama import OllamaLLM
 from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
 
 
-llm = OllamaLLM(model ="mistral", temperature= 0.7)
+llm = OllamaLLM(model = "phi4-mini")
 
 class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
 
-    message: Annotated[list[BaseMessage], add_messages]
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {"messages": [response]}
 
-
+# Checkpointer
 checkpointer = InMemorySaver()
 
 graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
 
-def chat_node(state: ChatState):
-    messages = state['message']
-    response = llm.invoke(messages)
-
-    return {"message": [response]}
-
-
-graph.add_node('chat_node', chat_node)
-
-graph.add_edge(START, 'chat_node')
-graph.add_edge('chat_node', END)
-
-
-workflow = graph.compile(checkpointer=checkpointer)
-print("workflow completed")
-print(workflow)
-
-
- 
-
+chatbot = graph.compile(checkpointer=checkpointer)
